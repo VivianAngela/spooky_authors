5b2df42a84bd6b7587ba11bb381808112b94055d
Jasper Brandes <jasperbrandes@jasperbsmacbook.mynet>
2017-11-05 17:39:29 +0000

    more text stats: punctuation, adj, adv count for each text snippet, gridsearch

--- bag_of_words_stats_ner_grid.py
+++ bag_of_words_stats_ner_grid.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[36]:
+# In[1]:
 
 import pandas as pd
 import numpy as np
@@ -27,91 +27,91 @@ import time
 import spacy
 from sklearn import preprocessing
 import warnings
-warnings.filterwarnings('ignore')
-get_ipython().magic('matplotlib inline')
+# warnings.filterwarnings('ignore')
+# get_ipython().magic('matplotlib inline')
 import utils
 
 
-# In[3]:
+# In[2]:
 
 nlp = spacy.load("en_core_web_md")
 
 
-# In[4]:
+# In[3]:
 
 train = pd.read_csv('data/train.csv')
 test  = pd.read_csv('data/test.csv')
 
 
-# In[5]:
+# In[4]:
 
 train.head(2)
 
 
-# In[6]:
+# In[5]:
 
 test.head(2)
 
 
-# In[7]:
+# In[6]:
 
 print("Number of labelled training examples: {}".format(train.shape[0]))
 
 
-# In[8]:
+# In[7]:
 
 print("Distribution of training data for the three authors")
-train['author'].value_counts().plot(kind="barh", color='brg')
+# train['author'].value_counts().plot(kind="barh", color='brg')
 
 
-# In[9]:
+# In[8]:
 
 print("Number of unlabelled test examples: {}".format(test.shape[0]))
 
 
-# In[10]:
+# In[9]:
 
 # simple split for screening model performance
 X_train, X_test, y_train, y_test = train_test_split(train, train.author, test_size=0.20, random_state=42)
 
 
-# In[11]:
+# In[10]:
 
 # n-fold stratified CV for robust model performance
 X = train
 y = train.author
 
 
-# In[12]:
+# In[11]:
 
 le = preprocessing.LabelEncoder()
 le.fit(y)
 
 
-# In[13]:
+# In[12]:
 
 le.classes_
 
 
-# In[14]:
+# In[13]:
 
 # sanity check for label ordering
 # 0: EAP, 1: HPL, 2: MWS
 
 
-# In[15]:
+# In[14]:
 
 y[:5]
 
 
-# In[16]:
+# In[15]:
 
 le.transform(y)[:5]
 
 
 # ## Author-specific Named Entities
 
-# In[17]:
+# In[16]:
 
 ner_EAP = pd.read_csv('data/ner_EAP.csv', header=None)
 ner_EAP = ner_EAP[0].tolist()
@@ -121,12 +121,12 @@ ner_MWS = pd.read_csv('data/ner_MWS.csv', header=None)
 ner_MWS = ner_MWS[0].tolist()
 
 
-# In[18]:
+# In[17]:
 
 ner_EAP[:5]
 
 
-# In[19]:
+# In[18]:
 
 def kfold_CV(clf, X, y, folds, transform=True):
     """ Run a stratified k-fold Cross Validation on the training set and print the results.
@@ -170,7 +170,7 @@ def kfold_CV(clf, X, y, folds, transform=True):
     print("Macro f1:  {}, Standard deviation: {}".format(np.mean(f1), np.std(f1)))
 
 
-# In[46]:
+# In[70]:
 
 class DataFrameColumnExtracter(BaseEstimator, TransformerMixin):
 
@@ -184,7 +184,7 @@ class DataFrameColumnExtracter(BaseEstimator, TransformerMixin):
         return X[self.column]
 
 
-# In[21]:
+# In[72]:
 
 class TextStats(BaseEstimator, TransformerMixin):
     """Extract features from each document for DictVectorizer"""
@@ -193,11 +193,15 @@ class TextStats(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, posts):
-        # print([{'length': len(text), 'num_sentences': text.count('.')} for text in posts])
-        return [{'length': len(text), 'num_sentences': text.count('.')} for text in posts]
+
+        return [{'length':len(nlp(text)),
+                 'num_sentences': len([sent for sent in nlp(text).sents]),
+                 'num_punctuations': len([token for token in nlp(text) if token.pos_ == "PUNCT"]),
+                 'num_descript_words': len([token for token in nlp(text) if token.pos_ == "ADJ" or token.pos_ == "ADV"])
+                } for text in posts]
 
 
-# In[22]:
+# In[73]:
 
 class NER_extractor(BaseEstimator, TransformerMixin):
     
@@ -226,7 +230,7 @@ class NER_extractor(BaseEstimator, TransformerMixin):
         return ner_list
 
 
-# In[76]:
+# In[74]:
 
 # Use FeatureUnion to combine the features
 featureunionvect = FeatureUnion(
@@ -260,42 +264,57 @@ featureunionvect = FeatureUnion(
 )
 
 
-# In[77]:
+# In[75]:
 
-classifier = MultinomialNB()
+classifier = LogisticRegression()
 
 
-# In[78]:
+# In[76]:
 
 pipeline = Pipeline([('vect', featureunionvect), ('classifier', classifier)])
 
 
-# In[95]:
+# In[77]:
 
-X_train_sample = X_train.head(100)
-y_train_sample = y_train.head(100)
+X_train_sample = X_train #.head(10)
+y_train_sample = y_train #.head(10)
 
 
-# In[100]:
+# In[78]:
+
+# pipeline.fit(X_train_sample, y_train_sample)
+
+
+# In[79]:
+
+# predicted = pipeline.predict(X_test)
+
+
+# In[80]:
+
+# print(classification_report(predicted, y_test))
+
+
+# In[67]:
 
 parameters = {
-    'vect__text_snippet__vec__max_df': (0.5, 0.625, 0.75, 0.875, 1.0),
+    'vect__text_snippet__vec__max_df': (0.5, 1.0), #0.625, 0.75, 0.875, 
     'vect__text_snippet__vec__max_features': (None, 5000, 10000, 20000),
-    'vect__text_snippet__vec__min_df': (1, 5, 10),#, 20, 50), 
+    'vect__text_snippet__vec__min_df': (1, 5, 10), #, 20, 50), 
     'vect__text_snippet__tfidf__use_idf': (True, False),
     'vect__text_snippet__tfidf__sublinear_tf': (True, False),
     'vect__text_snippet__vec__binary': (True, False),
     'vect__text_snippet__tfidf__norm': ('l1', 'l2'),
-    'classifier__alpha': (1, 0.1, 0.01, 0.001, 0.0001, 0.00001)  
+    'classifier': (MultinomialNB(), LogisticRegression(), LogisticRegression(), svm.LinearSVC(), svm.SVC())
     }  
 
 
-# In[101]:
+# In[68]:
 
-grid_search = GridSearchCV(pipeline, parameters, verbose=2)
+grid_search = GridSearchCV(pipeline, parameters, verbose=2, n_jobs=-1)
 
 
-# In[ ]:
+# In[69]:
 
 t0 = time.time()
 grid_search.fit(X_train_sample, y_train_sample)
@@ -305,4 +324,9 @@ print("Best parameters set:")
 best_parameters = grid_search.best_estimator_.get_params()
 for param_name in sorted(list(parameters.keys())):
     print("\t{0}: {1}".format(param_name, best_parameters[param_name]))
+
+
+# In[ ]:
+
+
 
